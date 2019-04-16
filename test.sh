9af584b19d745cc6736b8d07493adfc4a8da4c53
pli <pli01@users.noreply.github.com>
2019-04-16 15:33:11 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,10 @@
 set -x
 env |sort
 pwd
-apt-get install -qy nginx
+sudo apt-get install -qy nginx
 ifconfig
 
-( no_proxy=169.254.169.254 wget -q -O -  http://169.254.169.254/latest/meta-data/ )
+( no_proxy=169.254.169.254 curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip )
+sleep 60
  
 exit 0
