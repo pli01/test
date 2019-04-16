0a1d68b2c9f26e999a1a050e5f73882b862d1e3f
pli <pli01@users.noreply.github.com>
2019-04-16 15:51:59 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,8 @@ pwd
 sudo apt-get install -qy nginx
 ifconfig
 
-( no_proxy=169.254.169.254 curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip )
+ip=$(curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+echo $ip
 sleep 60
  
 exit 0
