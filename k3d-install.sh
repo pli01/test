8e8739d25dac9d6bcc3a9f9ca1c5c63d88071c48
pli01 <pli01@noreply.github.com>
2021-05-30 11:30:16 +0200

    k3d

---
+++ k3d-install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+set -o pipefail
+set -x
+
+curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=v4.4.4 bash
+
+k3d cluster create dev \
+   --port 8080:80@loadbalancer --port 8443:443@loadbalancer
+
+docker ps
+kubectl config current-context
+#export KUBECONFIG=$(k3d kubeconfig write dev)
+
+kubectl get nodes
+
+
+kubectl create deployment nginx --image=nginx
+kubectl create service clusterip nginx --tcp=80:80
+cat <<EOF | kubectl apply -f -
+apiVersion: networking.k8s.io/v1beta1
+kind: Ingress
+metadata:
+  name: nginx
+  annotations:
+    ingress.kubernetes.io/ssl-redirect: "false"
+spec:
+  rules:
+  - http:
+      paths:
+      - path: /
+        backend:
+          serviceName: nginx
+          servicePort: 80
+EOF
+
+curl --fail -s -L http://localhost:8080/
